4bf67b7054f1de9a2d5672e51bd050684b9a09d8
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-08 20:38:00 -0300

    Task e Action Results

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Models;
 
@@ -9,40 +11,40 @@ namespace Shop.Controllers
   {
     [HttpGet]
     [Route("")]
-    public string Get()
+    public async Task<ActionResult<List<Category>>> Get()
     {
-      return "Get";
+      return new List<Category>();
     }
 
     [HttpGet]
     [Route("{id:int}")]
-    public string GetById(int id)
+    public async Task<ActionResult<Category>> GetById(int id)
     {
-      return "id:" + id.ToString();
+      return new Category();
     }
 
     [HttpPost]
     [Route("")]
-    public Category Post([FromBody] Category model)
+    public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
     {
-      return model;
+      return Ok(model);
     }
 
     [HttpPut]
     [Route("{id:int}")]
-    public Category Put(int id, [FromBody] Category model)
+    public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
     {
       if (model.Id == id)
-        return model;
+        return Ok(model);
 
-      return null;
+      return NotFound();
     }
 
     [HttpDelete]
     [Route("{id:int}")]
-    public string Delete(int id)
+    public async Task<ActionResult<List<Category>>> Delete(int id)
     {
-      return "Deletar o id: " + id.ToString();
+      return Ok();
     }
 
   }
